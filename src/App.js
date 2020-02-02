f133201c8999d3a763bc9d046a59a28650cd9008
khalidCodeBucket <khalid.mahmud2014@gmail.com>
2020-02-02 11:24:13 +0600

    react recipe app

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import Form from './components/Form';
+import Recipes from './components/Recipes';
+
+const API_KEY = "16b3a474b7a052d326fcb1036e2b34ee";
+
+class App extends Component {
+
+  state = {
+    recipes: []
+  }
+
+  getRecipe = async (e) => {
+    const recipeName = e.target.elements.recipeName.value;
+    e.preventDefault();
+    const api_call = await fetch
+    (`https://cors-anywhere.herokuapp.com/http://www.food2fork.com/api/search?key=${API_KEY}&q=${recipeName}&count=10`);
+    // (`https://cors-anywhere.herokuapp.com/http://www.food2fork.com/api/search?key=${API_KEY}&q=shredded%20chicken&count=10`);
+    const data = await api_call.json();
+    this.setState({ recipes: data.recipes });
+  //  console.log(data.recipes[0].recipe_id);
+    console.log(this.state.recipes);
+  }
+
+  componentDidMount = () => {
+    const json = localStorage.getItem("recipes");
+    const recipes = JSON.parse(json);
+    this.setState({ recipes });
+  }
+  componentDidUpdate = () => {
+    const recipes = JSON.stringify(this.state.recipes);
+    localStorage.setItem("recipes", recipes);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Recipe Search</h1>
+        </header>
+        <Form getRecipe={this.getRecipe} />
+          <Recipes recipes={this.state.recipes}/>
+      </div>
+    );
+  }
+}
+
+export default App;
